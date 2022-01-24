c32e3baddf6fea1d16628065cc99cdbf5fd28469
韩鹏 <hanpeng@hotmaxx.cn>
2022-01-24 11:51:48 +0800

    test02

--- go-demo-serivce/test02.go
+++ go-demo-serivce/test02.go
@@ -7,4 +7,18 @@ func main()  {
 	var a int = 1
 	a = a + 20
 	fmt.Println(a)
+
+	b:=1
+	c:=3.14
+	fmt.Println(b,c)
+
+	d,e,f:=4,5,6
+	fmt.Println(d,e,f)
+
+	d,e,f=f,e,d
+	fmt.Println(d,e,f)
+
+	fmt.Printf("%d\n",d)
+	fmt.Printf("%s\n","hello word")
+
 }
